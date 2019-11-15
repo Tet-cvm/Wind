8cbb089d5932026825d19e4729b4b21e32b023c5
luxianming <luxianming@021.com>
2019-11-15 16:19:36 +0800

    id

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '172.21.0.3',
+    'hostname'        => 'cdb-j9x37b46',
     // 'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'movie',
