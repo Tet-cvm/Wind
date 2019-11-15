d64bc3f33a7b0bd31e3596e29e5271c6fff98c5f
luxianming <luxianming@021.com>
2019-11-15 16:48:57 +0800

    保存

--- application/database.php
+++ application/database.php
@@ -13,15 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '172.21.0.3',
-    // 'hostname'        => '127.0.0.1',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'movie',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'Slogger&90',
-    // 'password'        => '123456789',
+    'password'        => '123456789',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
